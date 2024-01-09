15b48eedf3702e6267daf0c70ef9397863738b60
Lucas Celestino <lucascap899@gmail.com>
2024-01-08 16:36:20 -0800

    ajustando test para função caesarCipher

--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -1,5 +1,5 @@
 import {caesarCipher} from './index'
 
 test('Deve retornar a string segundo a cifra de cesar', () => {
-    expect(caesarCipher('a')).toBe('d');
+    expect(caesarCipher('fulano teste')).toBe('ixodqr whvwh');
 })
